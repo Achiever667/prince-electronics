ea9d47d7260cfa108522217c030b68b46ff6f7fe
achiever667 <nwakaemmanuelchimezie@gmail.com>
2020-10-09 11:58:02 +0100

    done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 const profileimg = document.getElementById('contactimg').addEventListener('click', displayContacts);
-
+// var id = setInterval(frame, 30);
 function displayContacts(){
    document.querySelector('footer .container .row .col-sm  p').style.right = 0;
    document.querySelector('footer .container .row .col-sm  p').style.background = 'rgb(83, 188, 227)';
 
 }
 
-var windows =document.querySelector(' .container').addEventListener('hover', hideContacts);
+var windows =document.querySelector(' .container').addEventListener('click', hideContacts);
 function hideContacts(){
     document.querySelector('footer .container .row .col-sm  p').style.right = '-240px';
     document.querySelector('footer .container .row .col-sm  p').style.background = 'transparent';
 }
+
+//     myVar = setInterval(hideContacts, 3000);
+// clearInterval(myVar);
+// }
